af916f9cf59fa072cb01691a4c11c29d7f62052c
OlivierBigonnesse <olivier.bigonnesse@polymtl.ca>
2020-09-10 22:23:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,8 +6,8 @@ def majuscule(mot):
     for lettre in mot:
         # TODO completer la fonction ici
         if ord(lettre) >= 97 and ord(lettre) <= 122
-            lettre = ord(lettre) - distance_minuscule_majuscule
-            resultat += chr(lettre)
+        lettre = ord(lettre) - distance_minuscule_majuscule
+        resultat += chr(lettre)
     return resultat
 
 
